b4ca2d96882d2bc6e00cfa2894fe37f5b09d2566
marianafuego96-png <marianafuego96@gmail.com>
2026-01-19 22:12:44 -0300

    Completo el ejercicio4, modifiquel null por [] porque rompia

--- ej4.js
+++ ej4.js
@@ -91,7 +91,7 @@ let usuarios = [{
     id: 2,
     nombre: "MarianaFuego",
     email: "marianafuego96@hotmail.com",
-    librosPrestados: null
+    librosPrestados: []
 },
 {
     id: 3,
@@ -103,13 +103,13 @@ let usuarios = [{
     id: 4,
     nombre: "Manuel",
     email: "manuel@gmail.com",
-    librosPrestados: null
+    librosPrestados: []
 },
 {
     id: 5,
     nombre: "Pablo",
     email:"pablomartinez@gmail.com",
-    librosPrestados: null
+    librosPrestados: []
 }
 ]
 
@@ -121,4 +121,55 @@ let usuarios = [{
 a) Desarrollar una función prestarLibro(idLibro, idUsuario) que marque un libro como no disponible y lo agregue a la lista de libros prestados del usuario.
 b) Implementar una función devolverLibro(idLibro, idUsuario) que marque un libro como disponible y lo elimine de la lista de libros prestados del usuario. 
 */
+function prestarLibro(idLibro, idUsuario) {  // armamos la funcion de prestar libro y validamos id con usuario
+    const libro = libros.find(l => l.id === idLibro);
+    const usuario = usuarios.find(u => u.id === idUsuario);
+
+ if (!libro || !usuario) { // si el usuario o el libro no existen, se corta  y se avisa con el mensaje
+        console.log("Libro o usuario inexistente"); //  return evita que el codigo siga y rompa
+        return;
+    }
+
+    if (!libro.disponible) { // si el libro no esta disponible.. 
+        console.log("El libro no está disponible");
+        return;
+    }
+
+    libro.disponible = false; // no esta disponible
+
+    if (!Array.isArray(usuario.librosPrestados)) { // aca consultamos si array es realmente un array
+        usuario.librosPrestados = [];
+    }
+
+    usuario.librosPrestados.push(idLibro); // aca se agrega el libro
+
+}
+
+function devolverLibro (idLibro, idUsuario){
+    const libro = libros.find(l => l.id === idLibro); // se busca al libro por su id 
+    const usuario = usuarios.find(u => u.id === idUsuario); // se busca al usuario por su id
+
+    if (!libro || !usuario) { // por si no existe, te avisa
+        console.log("Libro o usuario inexistente");
+        return;
+    }
+// Validacion de si el usuario posee una lista, en caso que si, verifica si tiene o no ese libro prestado
+    if (!Array.isArray(usuario.librosPrestados) || !usuario.librosPrestados.includes(idLibro)) { 
+        console.log("El usuario no tiene este libro prestado");
+        return;
+    }
+// el true es devolver a la biblioteca
+    libro.disponible = true;
+    usuario.librosPrestados = usuario.librosPrestados.filter(id => id !== idLibro);
+}
+
+// pruebas de prestar y devolver libro  con id 8
+console.log("Antes:", usuarios[1], libros.find(l => l.id === 8));
+
+prestarLibro(8, 2);   // usuario 2 pide el libro 8
+console.log("Despues de prestar:", usuarios[1], libros.find(l => l.id === 8));
+
+devolverLibro(8, 2);  // usuario 2 devuelve el libro 8
+console.log("Despues de devolver:", usuarios[1], libros.find(l => l.id === 8));
+
 
